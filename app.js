0fade461e459d892c36f69f5c762960cdca39bca
kauafernandesandrade-sudo <kaua.fernandes.andrade@escola.pr.gov.br>
2025-10-10 11:50:33 -0300

    ok

---
+++ app.js
@@ -0,0 +1,14 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
